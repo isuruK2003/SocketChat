309e9f0d8c858603fae881b9b46285587aaa7fd3
Isuru Karunaratna <93091809+isuruK2003@users.noreply.github.com>
2025-01-01 16:03:40 +0530

    [update]message  changed from class to record

--- src/main/java/me/messages/Message.java
+++ src/main/java/me/messages/Message.java
@@ -1,27 +1,6 @@
 package me.messages;
 
-public class Message {
-	private String sender;
-	private String content;
-
-	public Message(String sender, String content) {
-		this.sender = sender;
-		this.content = content;
-	}
-
-	public String getSender() {
-		return this.sender;
-	}
-
-	public String getContent() {
-		return this.content;
-	}
-
-	@Override
-	public String toString() {
-		return "Message{"
-			+ "sender = " + this.sender
-			+ ", content = " + this.content
-			+ "}";
-	}
-}
+public record Message(
+	String sender,
+	String content
+) {}
